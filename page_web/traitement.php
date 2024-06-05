75ad0667b699a720f0731c508380cc142786c8b0
Matteo-K <kervadecmatteo@gmail.com>
2024-06-05 10:06:45 +0200

    Update html/css

--- page_web/traitement.php
+++ page_web/traitement.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body class="bg_nuage">
-
 <?php
 
     if (isset($_POST["Connexion_Test"]) || (isset($_POST["Inscription_Test"]))) {
@@ -22,14 +13,8 @@
             $conf_mdp = $_POST["conf_mdp_inscr"];
         }
 
-        
-
-
     } else {
         // Si l'utilisateur a réussi à accéder à la page sans cliquer sur le bouton
         echo "Erreur : Vous n'avez pas la permission d'accès à cette page";
     }
-    ?>
-
-</body>
-</html>
+?>
